e075ed2427c3226ba58e1a43212e484cf6d03e2b
mavolin <github@mavolin.co>
2023-02-20 11:58:28 +0100

    fix(hashets): only invoke `Options.Ignore` on filepaths, as guaranteed in its doc

--- hashets/hashets.go
+++ hashets/hashets.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/fs"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 )
@@ -41,21 +42,17 @@ func Hash(inFS fs.FS, o Options) (Map, error) {
 	o.setDefaults()
 
 	m := make(Map)
-	err := fs.WalkDir(inFS, ".", func(path string, dir fs.DirEntry, _ error) error {
-		path = strings.TrimPrefix(path, "./")
+	err := fs.WalkDir(inFS, ".", func(p string, dir fs.DirEntry, _ error) error {
+		p = strings.TrimPrefix(p, "./")
 		if dir == nil || dir.IsDir() {
-			if o.Ignore(path) {
-				return fs.SkipDir
-			}
-
 			return nil
 		}
 
-		if o.Ignore(path) {
+		if o.Ignore(p) {
 			return nil
 		}
 
-		in, err := inFS.Open(path)
+		in, err := inFS.Open(p)
 		if err != nil {
 			return err
 		}
@@ -70,9 +67,9 @@ func Hash(inFS fs.FS, o Options) (Map, error) {
 			return err
 		}
 
-		name := filepath.Base(path)
+		name := path.Base(p)
 		hash := o.Hash.Sum(nil)
-		m[path] = path[:len(path)-len(name)] + o.NamingFunc(name, o.HashToText(hash))
+		m[p] = p[:len(p)-len(name)] + o.NamingFunc(name, o.HashToText(hash))
 		return nil
 	})
 	if err != nil {
@@ -157,10 +154,6 @@ func HashToDir(inFS fs.FS, outPath string, o Options) (Map, error) {
 				}
 			}
 
-			if o.Ignore(path) {
-				return fs.SkipDir
-			}
-
 			return nil
 		}
 
@@ -197,7 +190,7 @@ func writeHashed(inFS fs.FS, inPath, outPath string, m Map, o Options) error {
 		return err
 	}
 
-	name := filepath.Base(inPath)
+	name := path.Base(inPath)
 	hash := o.Hash.Sum(nil)
 	hashedPath := inPath[:len(inPath)-len(name)] + o.NamingFunc(name, o.HashToText(hash))
 	m[inPath] = hashedPath
@@ -212,7 +205,7 @@ func writeHashed(inFS fs.FS, inPath, outPath string, m Map, o Options) error {
 		return err
 	}
 
-	out, err := os.OpenFile(outPath+"/"+hashedPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, stat.Mode()&0o555)
+	out, err := os.OpenFile(filepath.Join(outPath, hashedPath), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, stat.Mode()&0o555)
 	if err != nil {
 		return err
 	}
